44d4a1406dca184484b7341bc566d60f6eb5e886
Alex <gwartneydev@gmail.com>
2021-01-19 23:44:53 -0800

    added in catalog

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php 
+$pageTitle = "Personal Media Library";
+$section = null;
+
+include("inc/header.php"); ?>
+		<div class="section catalog random">
+
+			<div class="wrapper">
+
+				<h2>May we suggest something?</h2>
+
+        <ul class="items">
+            <li>
+                <a href="#">
+                    <img src="img/media/design_patterns.jpg" alt="Design Patterns">
+                    <p>View Details</p>
+                </a>
+            </li><li>
+                <a href="#">
+                    <img src="img/media/forest_gump.jpg" alt="Forest Gump">
+                    <p>View Details</p>
+                </a>
+            </li><li>
+                <a href="#">
+                    <img src="img/media/beethoven.jpg" alt="Beethoven">
+                    <p>View Details</p>
+                </a>
+            </li><li>
+                <a href="#">
+                    <img src="img/media/clean_code.jpg" alt="Clean Code">
+                    <p>View Details</p>
+                </a>
+            </li>								
+				</ul>
+
+			</div>
+
+		</div>
+
+<?php include("inc/footer.php"); ?>
