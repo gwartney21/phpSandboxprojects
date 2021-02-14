dedcbf1beeb4b3eef2cad7c6fee30944e0712026
Alexander <gwartneydev@gmail.com>
2021-02-14 03:03:46 -0800

    added in create tasklist

--- Crud/inc/functions.php
+++ Crud/inc/functions.php
@@ -17,8 +17,7 @@ function get_project_list(){
 function get_task_list(){
     include 'connection.php';
   
-    $sql =  'SELECT tasks.*, projects.title as project FROM tasks'
-            .'JOIN projects ON tasks.project_id = projects.project_id';
+    $sql = 'SELECT tasks.*, projects.title as project FROM tasks JOIN projects ON tasks.project_id = projects.project_id';
     try{
         return $db->query($sql);
     }catch (Exception $e){
@@ -43,4 +42,26 @@ function add_project($title, $category){
     } catch(Exception $e){
         echo "Error!:" . $e->getMessage() . "<br />";
     }
+
+    return true;
+}
+
+
+function add_task($project_id, $title, $date, $time){
+    include 'connection.php';
+
+    $sql = 'INSERT INTO tasks(project_id, title, date, time) VALUES(?,?,?,?)';
+    
+    try{
+        $results = $db->prepare($sql);
+        $results->bindValue(1, $project_id,  PDO::PARAM_INT);
+        $results->bindValue(2, $title, PDO::PARAM_STR);
+        $results->bindValue(3, $date,  PDO::PARAM_STR);
+        $results->bindValue(4, $time, PDO::PARAM_INT);
+        $results->execute();
+    } catch(Exception $e){
+        echo "Error!:" . $e->getMessage() . "<br />";
+    }
+
+    return true;
 }
